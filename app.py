13f7cfb59a47e8dd4e78839f89be01659ba26c78
william-zhang-ml <william.zhang.ml@gmail.com>
2024-07-04 11:39:17 -0400

    add blank app

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+This app helps you plan your path to your next stage of personal finance.
+It is a dashboard that shows you the effects of tuning different numbers.
+The exact toolset presented will depend on where you're currently at.
+The UI will evolve/unlock new tools as you progress through different stages.
+"""
+import tkinter as tk
+
+
+if __name__ == '__main__':
+    # create main dashboard window
+    ROOT = tk.Tk()
+    ROOT.title('Stage 0')
+    ROOT.mainloop()
