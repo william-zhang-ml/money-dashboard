aadebb09827dec0f56908946c2cf3743932afd3a
william-zhang-ml <william.zhang.ml@gmail.com>
2024-07-08 17:16:16 -0400

    update DebtPayoffWidget to use LineGraph for graphics

--- widgets.py
+++ widgets.py
@@ -8,13 +8,29 @@ import utils
 class DebtPayoffWidget:
     """Widget for showing how APR and payment amount impact payoff time. """
     def __init__(self, dashboard: tk.Tk) -> None:
-        self.fig, self.axes = plt.subplots(figsize=(4, 4))
-        self.axes.set_title('Debt Payoff Schedule')
-        self.axes.grid()
-        self.canvas = FigureCanvasTkAgg(self.fig, dashboard)
+        self.linegraph = utils.LineGraph()
+        self.canvas = FigureCanvasTkAgg(self.linegraph.fig, dashboard)
 
-        _, running_balance = utils.calc_balance_over_time()
-        self.axes.plot(running_balance, '.-')
+        # scenario 1
+        _, running_balance = utils.calc_balance_over_time(
+            balance=1000,
+            payment=50,
+            apr=25
+        )
+        self.linegraph.plot(running_balance, '.-')
+
+        # scenario 2
+        _, running_balance = utils.calc_balance_over_time(
+            balance=1000,
+            payment=100,
+            apr=25
+        )
+        self.linegraph.plot(running_balance, '.-')
+
+        self.linegraph.axes.set_title('Debt Payoff Schedule')
+        self.linegraph.axes.grid()
+        self.linegraph.axes.set_xlabel('months')
+        self.linegraph.axes.set_ylabel('balance')
 
     def pack(self, *args, **kwargs) -> None:
         """Pack the widget in a parent widget (refer to tkinter docs). """
