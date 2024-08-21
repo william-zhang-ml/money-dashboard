88cd75ca6c82a71af04398afb38ff64db93cc2c6
William Zhang <william.zhang.ml@gmail.com>
2024-08-20 23:29:27 -0400

    update main app to use tabs for each widget

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ The exact toolset presented will depend on where you're currently at.
 The UI will evolve/unlock new tools as you progress through different stages.
 """
 import tkinter as tk
-from widgets import BudgetWidget
+from tkinter import ttk
+from widgets import (
+    BudgetWidget,
+    DebtPayoffWidget,
+    FireWidget
+)
 
 
 if __name__ == '__main__':
     # create main dashboard window
     ROOT = tk.Tk()
-    ROOT.title('Stage 0')
-    BudgetWidget(ROOT).pack(expand=True, fill=tk.BOTH, padx=2, pady=2)
+    ROOT.title('Money Dashboard')
+    tabs = ttk.Notebook(ROOT)
+    tabs.add(BudgetWidget(tabs), text='Budget')
+    tabs.add(DebtPayoffWidget(tabs), text='Debt')
+    tabs.add(FireWidget(tabs), text='Wealth')
+    tabs.pack(expand=True, fill=tk.BOTH, padx=2, pady=2)
     ROOT.mainloop()
